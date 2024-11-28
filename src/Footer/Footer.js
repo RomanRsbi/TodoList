49b97e2adb499aeb7c84cdb0506d6b8fe144d040
Roman <romanrsbi@gmail.com>
2024-11-28 16:00:41 +0300

    test commit

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import TaskFilter from '../TaskFilter/TaskFilter';
 import './Footer.css';
@@ -7,7 +7,7 @@ import './Footer.css';
 export default function Footer({ buttonType = 'All', checkBtnType = () => {}, deleteAll = () => {}, activeCount = 0 }) {
   let countInfo = '';
   if (activeCount === 0) {
-    countInfo += 'no active task';
+    countInfo += 'no active task'
   }
   if (activeCount !== 0) {
     countInfo += `${activeCount} items left`;
