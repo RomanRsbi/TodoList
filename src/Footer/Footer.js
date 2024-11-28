b36af791cf72365a2bd156803ce69784545c5a41
Roman <romanrsbi@gmail.com>
2024-11-28 15:52:32 +0300

    test husky

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 import TaskFilter from '../TaskFilter/TaskFilter';
 import './Footer.css';
